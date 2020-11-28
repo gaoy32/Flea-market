ff7fbdbdd616d888220c6c01e70708443384dc9c
gaoy32 <61880796+gaoy32@users.noreply.github.com>
2020-11-29 04:15:37 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+//import Amplify from 'aws-amplify'
+//import config from './aws-exports'
+
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import AppNavigator from "./navigation/AppNavigator"
+
+import { View, Text} from "react-native";
+
+//Amplify.configure(config)
+
+export default function App() {
+  return (
+    <NavigationContainer>
+       <AppNavigator />
+    </NavigationContainer>
+    // <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    //   <Text>Home Screen</Text>
+    // </View>
+  );
+}
